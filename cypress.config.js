4278c49899dffb5806d4857328fe502e8ec47240
Youssef Elsakhi <youssefsakhi54@gmail.com>
2022-07-03 12:19:18 -0400

    add mochaawsome reporter to cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -5,10 +5,20 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       return require('./cypress/plugins/index.js')(on, config)
     },
     baseUrl: 'https://www.amazon.com/',
-    retries: 2
-
+    retries: 2,
+    videoCompression: false,
+    screenshotsFolder: "cypress/reports/screenshots",
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      charts: true,
+      reportPageTitle: 'custom-title',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+    },
   },
 })
