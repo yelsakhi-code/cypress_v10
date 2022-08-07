c31b623f966a73f9543e9582d60f7c085feabc5e
Zeroual <ElMokhtar.Zeroual@purina.nestle.com>
2022-08-07 00:25:01 -0400

    YoutuveVideo

--- cypress/e2e/Assertion/AssertionFacbook.cy.js
+++ cypress/e2e/Assertion/AssertionFacbook.cy.js
@@ -1,8 +1,40 @@
 
-describe('intecept', {​​​​​​baseUrl: 'https://www.facebook.com/'}​​​​​​,  () => {​​​​​​ 
-   beforeEach(() => {​​​​​​           cy.visit('/');    }​​​​​​)    
- it('Title and URL Assertions ', () => {​​​​​​  
-          cy.url().should('eq', 'https://www.facebook.com/')    
- }​​​​​​);   
- }​​​​​​);
+describe('Home Tests',  {baseUrl: 'https://www.google.com/'},  () => {​​​​​​ 
+ //  beforeEach(() => {​​​​​​      
+        cy.visit('https://practice.automationbro.com/');    }​​​​​​)    
+   ///
+   it("Open The home page and verify the urk and the title", () => {​​​​​​ 
+          cy.visit('/');
+
+        cy.visit('https://practice.automationbro.com/');  
+
+
+        cy.url().should("include", "automationbro"); 
+
+
+        cy.title().should("eq", "Parctice E-Commerce site - Automation Bro")    
+ }​​​​​​); 
+
+ ///
+ 
+ it(' Clicks the get Started buttonand asserts the url', () =>{
+
+    cy.get("#get-started").click();
+
+    cy.url().should("include", "#get-started");
+
+ });
+///
+  it(' Get the text of the heading and assert the value', () =>{ 
+
+      cy.get("H1.elementor-heading-title").should("have.text", "Think diferent. Make different")
+
+  });
+
+  it('verifies the text of the first menu link item', () =>{
+
+    cy.get("#primary-menu").find("li").first().should("have.text", "home");
+
+  
+ }​​​​​​)
 
